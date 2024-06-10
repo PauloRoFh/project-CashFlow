ce0b86ed71d839da94f0d3959c4663abf8823ea7
paulo.rofh <paulo.fh@icloud.com>
2024-06-10 13:52:12 -0300

    Improved error handling and data validation in expense registration
    In this commit, we have enhanced the error handling and data validation in the expense registration process. In `ExpensesController.cs`, the `Register` method is now wrapped in a try-catch block to handle exceptions from `RegisterExpenseUseCase`. It returns more informative responses based on the type of exception.
    
    In `RegisterExpenseUseCase.cs`, a new `Validade` method has been added to validate the `RequestRegisterExpenseJson` request. It checks for empty title, negative or zero amount, future date, and valid payment type. If any check fails, it throws an `ArgumentException`. The `Execute` method now calls this `Validade` method before proceeding with the use case execution.
    
    Finally, the `CashFlow.Communication.Enums` namespace has been added to the using directives in `RegisterExpenseUseCase.cs` to use the `PaymentType` enum in the `Validade` method.

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,6 +7,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validade(request);
+
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validade(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+
+        var resultDateTime = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (resultDateTime > 0)
+        {
+            throw new ArgumentException("The date must be less than or equal to the current date");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("The payment type is invalid");
+        }
     }
 }
